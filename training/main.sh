9d7f39ec001e945e920840afd27727c0247732e6
kopi <kopal.jakub@gmail.com>
2019-11-24 14:47:24 +0100

    pc finished

--- training/main.sh
+++ training/main.sh
@@ -73,76 +73,92 @@ INPUT_TENSORS='normalized_input_image_tensor'
 OUTPUT_TENSORS='TFLite_Detection_PostProcess,TFLite_Detection_PostProcess:1,TFLite_Detection_PostProcess:2,TFLite_Detection_PostProcess:3'
 
 cd "${LOCAL_GIT}/object_detection_api/"
-#source "/home/kopi/tools/enviroments/tf_gpu/bin/activate"
-
-for CKPT_DIR in ~/diplomka/training/configs/configs_to_process/*/ ; do
 
 
-    echo "CONVERTING dataset to TF Record..."
-    python object_detection/dataset_tools/create_pet_tf_record2.py \
-        --data_dir="${DATASET}" \
-        --output_dir="${LOCAL_GIT}/dataset/exported/tf_records"
+# number_to_process=$(ls "${PROJECT_ROOT}/training/configs/configs_to_process/" | wc -l)
 
-    echo "tf records done"
+# echo ${number_to_process}
+# exit
 
-    CKPT_NAME="$(basename $CKPT_DIR)"
-    OUTPUT_I="${PROJECT_ROOT}/training/output/${CKPT_NAME}"
-    if [ -d "${OUTPUT_I}" ]; then rm -Rf ${OUTPUT_I}; fi
-    mkdir "${OUTPUT_I}"
+number_to_process=$(ls "${PROJECT_ROOT}/training/configs/configs_to_process/" | wc -l)
+if [ "${number_to_process}" != '0' ]; then
+    for CKPT_DIR in ~/diplomka/training/configs/configs_to_process/*/ ; do
 
 
+        echo "CONVERTING dataset to TF Record..."
+        python object_detection/dataset_tools/create_pet_tf_record2.py \
+            --data_dir="${DATASET}" \
+            --output_dir="${LOCAL_GIT}/dataset/exported/tf_records"
 
-    copy_model_into_obj_api "${CKPT_DIR}" "${HOME}/local_git/architectures"
+        echo "tf records done"
 
-    TRAIN_DIR="${OUTPUT_I}/train"
-    echo "Start training..."
-    python object_detection/model_main.py \
-        --pipeline_config_path="${CKPT_DIR}/pipeline.config" \
-        --model_dir="${TRAIN_DIR}" \
-        --num_train_steps="${num_training_steps}"
-        #--num_eval_steps="1000"
+        CKPT_NAME="$(basename $CKPT_DIR)"
+        OUTPUT_I="${PROJECT_ROOT}/training/output/${CKPT_NAME}"
+        if [ -d "${OUTPUT_I}" ]; then rm -Rf ${OUTPUT_I}; fi
+        mkdir "${OUTPUT_I}"
 
 
 
-    MODEL_DIR="${OUTPUT_I}/model"
-    ckpt_number=${num_training_steps}
-    echo "EXPORTING frozen graph from checkpoint..."
-    python object_detection/export_tflite_ssd_graph.py \
-        --pipeline_config_path="${CKPT_DIR}/pipeline.config" \
-        --trained_checkpoint_prefix="${TRAIN_DIR}/model.ckpt-${ckpt_number}" \
-        --output_directory="${MODEL_DIR}" \
-        --add_postprocessing_op=true
+        copy_model_into_obj_api "${CKPT_DIR}" "${HOME}/local_git/architectures"
+
+        TRAIN_DIR="${OUTPUT_I}/train"
+        echo "Start training..."
+        python object_detection/model_main.py \
+            --pipeline_config_path="${CKPT_DIR}/pipeline.config" \
+            --model_dir="${TRAIN_DIR}" \
+            --num_train_steps="${num_training_steps}"
+            #--num_eval_steps="1000"
 
 
-    # Parse width and height from pipeline.config file
-    HEIGHT=$(cat "${CKPT_DIR}/pipeline.config" | grep "height:" | sed "s/[a-z]*://g")
-    WIDTH=$(cat "${CKPT_DIR}/pipeline.config" | grep "width:" | sed "s/[a-z]*://g")
 
-    echo "CONVERTING frozen graph to TF Lite file..."
-    tflite_convert \
-        --output_file="${MODEL_DIR}/output_tflite_graph.tflite" \
-        --graph_def_file="${MODEL_DIR}/tflite_graph.pb" \
-        --inference_type=QUANTIZED_UINT8 \
-        --input_arrays="${INPUT_TENSORS}" \
-        --output_arrays="${OUTPUT_TENSORS}" \
-        --mean_values=128 \
-        --std_dev_values=128 \
-        --input_shapes=1,"${HEIGHT}","${WIDTH}",3 \
-        --change_concat_input_ranges=false \
-        --allow_nudging_weights_to_use_fast_gemm_kernel=true \
-        --allow_custom_ops
+        MODEL_DIR="${OUTPUT_I}/model"
+        ckpt_number=${num_training_steps}
+        echo "EXPORTING frozen graph from checkpoint..."
+        python object_detection/export_tflite_ssd_graph.py \
+            --pipeline_config_path="${CKPT_DIR}/pipeline.config" \
+            --trained_checkpoint_prefix="${TRAIN_DIR}/model.ckpt-${ckpt_number}" \
+            --output_directory="${MODEL_DIR}" \
+            --add_postprocessing_op=true
 
-    # Compile model for edge tpu
-    edgetpu_compiler "${MODEL_DIR}/output_tflite_graph.tflite" -o "${MODEL_DIR}"
 
-    # Delete all unnessesary model files
-    delete_train_files "${TRAIN_DIR}"
-    delete_model_files "${MODEL_DIR}"
+        # Parse width and height from pipeline.config file
+        HEIGHT=$(cat "${CKPT_DIR}/pipeline.config" | grep "height:" | sed "s/[a-z]*://g")
+        WIDTH=$(cat "${CKPT_DIR}/pipeline.config" | grep "width:" | sed "s/[a-z]*://g")
 
-    echo "---Additional informations---" >> "${MODEL_DIR}/output_tflite_graph_edgetpu.log"
-    echo "width: ${WIDTH}" >> "${MODEL_DIR}/output_tflite_graph_edgetpu.log"
-    echo "height: ${HEIGHT}" >> "${MODEL_DIR}/output_tflite_graph_edgetpu.log"
+        echo "CONVERTING frozen graph to TF Lite file..."
+        tflite_convert \
+            --output_file="${MODEL_DIR}/output_tflite_graph.tflite" \
+            --graph_def_file="${MODEL_DIR}/tflite_graph.pb" \
+            --inference_type=QUANTIZED_UINT8 \
+            --input_arrays="${INPUT_TENSORS}" \
+            --output_arrays="${OUTPUT_TENSORS}" \
+            --mean_values=128 \
+            --std_dev_values=128 \
+            --input_shapes=1,"${HEIGHT}","${WIDTH}",3 \
+            --change_concat_input_ranges=false \
+            --allow_nudging_weights_to_use_fast_gemm_kernel=true \
+            --allow_custom_ops
 
-done
+        # Compile model for edge tpu
+        edgetpu_compiler "${MODEL_DIR}/output_tflite_graph.tflite" -o "${MODEL_DIR}"
+
+        # Delete all unnessesary model files
+        delete_train_files "${TRAIN_DIR}"
+        delete_model_files "${MODEL_DIR}"
+
+        echo "---Additional informations---" >> "${MODEL_DIR}/output_tflite_graph_edgetpu.log"
+        echo "width: ${WIDTH}" >> "${MODEL_DIR}/output_tflite_graph_edgetpu.log"
+        echo "height: ${HEIGHT}" >> "${MODEL_DIR}/output_tflite_graph_edgetpu.log"
+
+    done
+fi
+
+
+cd ${PROJECT_ROOT}
+git add -A
+git commit -m 'pc finished'
+git push
+
+shutdown now
 
 
